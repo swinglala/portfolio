81b8b4a0df72ea45e196f6dfc2ffad9eb4275b2e
SoominSeo <wishmin82@tosky.co.kr>
2025-09-02 13:21:55 +0900

    init.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,3 @@ export default {
   },
   plugins: [],
 };
-
-module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-};
